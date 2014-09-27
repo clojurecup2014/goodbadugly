8efa441fa4f368a5fa74d3ac321bf4fb62840e6f
Juan Facorro <juan.facorro@gmail.com>
2014-09-26 22:47:12 -0300

    [#2] Added deps for website. Routes and web app creation.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject gbu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Check your Clojure code with Eastwood."
+  :url "http://goodbadugly.clojurecup.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [enlive "1.1.5"]
+                 [hiccup "1.0.5"]
+                 [ring/ring-jetty-adapter "1.2.2"]])
--- src/gbu/core.clj
+++ src/gbu/core.clj
@@ -1,6 +1,20 @@
-(ns gbu.core)
+(ns gbu.core
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn home []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello"})
+
+(defroutes app
+  (GET "/" []
+    (home))
+  (ANY "*" []
+    (route/not-found "Oops 404")))
+
+(defn -main [& [port]]
+  (let [port (int 5000)]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
