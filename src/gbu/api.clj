8bedef5632cdbab3edc5c59d518c5130e98540d0
Juan Facorro <juan.facorro@gmail.com>
2014-12-06 15:37:45 -0300

    Added docstrings to the api functions.

--- src/gbu/api.clj
+++ src/gbu/api.clj
@@ -18,6 +18,7 @@
    :content_type "json"})
 
 (defn- qs-map
+  "Given a querystring builds a map with its keys and values."
   [qs]
   (let [split (fn [x s] (map str (.split x s)))]
     (->> (split qs "&")
@@ -25,11 +26,15 @@
       (map vec)
       (into {}))))
 
-(defn token [cookies]
+(defn token
+  "Given the map of cookies returns the value of
+the GitHub's token if present or nil otherwise."
+  [cookies]
   (when-let [{:keys [value]} (cookies "token")]
     value))
 
 (defn login
+  "Starts the OAuth process."
   []
   (let [url (str authorize-url
               "?client_id=" client-id
@@ -38,21 +43,26 @@
    :headers {"location" url}}))
 
 (defn callback
+  "Handles the callback from GitHub OAuth flow."
   [code]
   (let [params {:form-params {:client_id client-id
                               :client_secret client-secret
                               :code code}}
         {:keys [body]} (client/post access-token-url params)
-        token  ((qs-map body)"access_token")]
+        token  ((qs-map body) "access_token")]
     {:status 302
      :headers {"location" "/repos"
                "set-cookie" (str "token=" token ";Path=/")}}))
 
 (defn- owner?
+  "Returns true if the user has admin permissions over the 
+repo and false otherwise."
   [repo]
   (get-in repo [:permissions :admin]))
 
 (defn- get-webhook
+  "Given a repository checks if it has a specific webhook
+enables. Returns the webhook if present or nil otherwise."
   [token user reponame]
   (let [hooks        (repos/hooks user reponame {:oauth-token token})
         gbu-webhook? #(= (:url webhook-config)
@@ -63,6 +73,8 @@
 
 
 (defn- add-status
+  "Given a repo adds the status of the webhook as either
+:on of :off in the :gbu key."
   [token repo]
   (let [user     (get-in repo [:owner :login])
         reponame (:name repo)
@@ -70,6 +82,8 @@
     (assoc repo :gbu status)))
 
 (defn- all-user-repos
+  "Returns a list of repositories where the user
+has the necessary permissions to create a webhook."
   [token]
   (let [opts       {:oauth-token token
                     :type :owner}
@@ -81,6 +95,8 @@
       (map (partial add-status token)))))
 
 (defn repos
+  "Returns all of the repos in which the user has
+permission for creating webhooks."
   [cookies]
   (if-let [token (token cookies)]
     {:status 200
@@ -89,6 +105,7 @@
      :body "Token missing."}))
 
 (defn- create-webhook
+  "Create a webhook in the specified repo."
   [token user repo]
   (let [opts    {:oauth-token token
                  :active true
@@ -96,6 +113,10 @@
     (repos/create-hook user repo "web" webhook-config opts)))
 
 (defn- add-gbu-user
+  "Add the GBU user to the repository. 
+TODO: for private repositories the user should be
+either added as a collaborator (user repo) or as 
+part of a team (org repo)."
   [token repo]
   (let [owner (:owner repo)]
     (if (= (:type owner) "Organization")
@@ -105,6 +126,7 @@
       (prn "Add user as collaborator"))))
 
 (defn on
+  "Enables the webhook for the specified repository."
   [cookies user reponame]
   (let [token   (token cookies)
         repo    (repos/specific-repo user reponame {:oauth-token token})]
@@ -115,6 +137,7 @@
      :body "done"}))
 
 (defn off
+  "Disables the webhook for the specified repository."
   [cookies user reponame]
   (let [token   (token cookies)
         webhook (get-webhook token user reponame)]
